7b03cd53d8c826ac7f743109df858afc6d466965
oburacarol <carologira8@gmail.com>
2025-11-24 17:02:38 +0100

    Error fixed: 'no module named experiment'. Fix -> path to src explicitly (Windows version)  added

--- src/experiment/run_ihdp.sh
+++ src/experiment/run_ihdp.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# Set up the Python path to include src directory (fix for running from bash)
+export PYTHONPATH="G:/My Drive/CML_Research/Practice Code/dragonnet/src"
 
 options=(
     dragonnet
-    tarnet
+    #tarnet
 
 )
 
@@ -11,9 +13,9 @@ options=(
 
 for i in ${options[@]}; do
     echo $i
-    python -m experiment.ihdp_main --data_base_dir /Users/claudiashi/ml/dragonnet/dat/ihdp/csv\
+    python -m experiment.ihdp_main --data_base_dir dat/ihdp/csv\
                                  --knob $i\
-                                 --output_base_dir /Users/claudiashi//ml/dragonnet/result/ihdp\
+                                 --output_base_dir co_implement/ihdp\
 
 
 done
