287bd21c5e24bb606d00fe3c74dad5091a97d6f3
oburacarol <carologira8@gmail.com>
2025-11-24 17:06:38 +0100

    rmsprop removed : not used in the script, and is also an outdated import name

--- src/experiment/ihdp_main.py
+++ src/experiment/ihdp_main.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import keras.backend as K
-from keras.optimizers import rmsprop, SGD, Adam
+from keras.optimizers import SGD, Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau, TerminateOnNaN
 from experiment.idhp_data import *
 
